32a794b043bec4eff6eaacc98d02e22e860821e6
matheus-mcosta <matheus.costa@inf.ufrgs.br>
2024-11-06 23:04:21 -0300

    feat: bench to csv

--- trab1/bench.sh
+++ trab1/bench.sh
@@ -1,10 +1,34 @@
 #!/bin/bash
 
 TARGET="$1"
+HEADER_SET=false
+EXPECTED=0
+
+benchmark() {
+	local FILE="$1"
+	for _ in {1..10}; do
+
+		local OUTPUT=$(./"$TARGET" "$FILE")
+
+		if [ "$HEADER_SET" = false ]; then
+			echo "name,$(echo "$OUTPUT" | head -n 1 | awk '{$1=$1; print}' OFS=',')" > output.csv
+			HEADER_SET=true
+			EXPECTED=$(echo "$OUTPUT" | tail -n 1)
+		fi
+
+		echo "$FILE,$(echo "$OUTPUT" | head -n 2 | tail -n 1 | awk '{$1=$1; print}' OFS=',')" >> output.csv
+		VALUE=$(echo "$OUTPUT" | tail -n 1)
+		if ! awk "BEGIN {exit !($VALUE == $EXPECTED)}"; then
+			echo "Expected $EXPECTED but got $VALUE"
+			exit 1
+		fi
+	done
+}
 
 echo "Benchmarking"
-./"$TARGET" "data.csv" &&
-    ./"$TARGET" "sorted_id.csv" &&
-    ./"$TARGET" "sorted_timestamp.csv" &&
-    ./"$TARGET" "sorted_value.csv" &&
-    ./"$TARGET" "sorted_valid.csv"
+
+benchmark "data.csv"
+benchmark "sorted_id.csv"
+benchmark "sorted_timestamp.csv"
+benchmark "sorted_value.csv"
+benchmark "sorted_valid.csv"
